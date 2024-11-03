123a66345bf9906c9c04c772a75e015b60f9f693
LathorTylla <fabianmurillo1802@gmail.com>
2024-11-03 00:41:46 -0600

    NavMesh and NavMeshAgent
    NavMeshAgent and NavMeshSurf
    Added:
    
    The player can follow and object following the NavMeshSurface
    
    The player can jump between 2 objects
    
    The player can walk under a bridge

---
+++ Assets/scrips/Navigation/NavigationMeshAgent.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationMeshAgent : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+    private
+    NavMeshAgent anvMeshAgent;
+
+    private
+    NavMeshAgent navMeshAgent;
+
+    private
+    void
+    Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private
+    void
+    Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
